4985b9d1e5f780f6413b8cb1e6e034d6dc515607
Shakeer-C <chilakalashakeer33@gmail.com>
2025-05-13 00:34:04 +0530

    Add files via upload

--- Project-files/src/main/java/com/LMS_Java/dao/DAO_Admin_Request.java
+++ Project-files/src/main/java/com/LMS_Java/dao/DAO_Admin_Request.java
@@ -19,6 +19,11 @@ public class DAO_Admin_Request {
 	private static final String JDBC_USER = "root";
 	private static final String JDBC_PASSWORD = "Shakeer@123";
 
+	private static int batchCount;
+	private static int teacherCount;
+	private static int studentCount;
+
+
 //------------------------------request_Admin---------------------------------------
 	public boolean request_Admin(Object modal, String ad_Req) {
 		boolean status = false;
@@ -38,6 +43,18 @@ public class DAO_Admin_Request {
 				status = insert_Teacher((MD_ST_Request) modal, reqCon);
 			} else if ("studentRegister".equalsIgnoreCase(ad_Req)) {
 				status = insert_Student((MD_ST_Request) modal, reqCon);
+			} else if ("teacherUpdate".equalsIgnoreCase(ad_Req)) {
+				status = update_Teacher((MD_Admin_GetList) modal, reqCon);
+			} else if ("studentUpdate".equalsIgnoreCase(ad_Req)) {
+				status = update_Student((MD_Admin_GetList) modal, reqCon);
+			} else if ("teacherDelete".equalsIgnoreCase(ad_Req)) {
+				status = delete_Teacher((MD_Admin_GetList) modal, reqCon);
+			} else if ("studentDelete".equalsIgnoreCase(ad_Req)) {
+				status = delete_Student((MD_Admin_GetList) modal, reqCon);
+			}else if ("stMWinsert".equalsIgnoreCase(ad_Req)) {
+				status = stMW_Insert((MD_Admin_GetList) modal, reqCon);
+			}else if ("stMWupdate".equalsIgnoreCase(ad_Req)) {
+				status = stMW_Update((MD_Admin_GetList) modal, reqCon);
 			} else {
 				System.out.println("Admin_Request Failed: Unknown Request Type");
 			}
@@ -71,9 +88,8 @@ public class DAO_Admin_Request {
 			int row = ps.executeUpdate();
 			if (row > 0) {
 				status = true;
-				System.out.println(mar.getAdName() + " Admin_Registration successful!");
+
 			} else {
-				System.out.println(mar.getAdName() + " Admin_Registration failed!");
 			}
 		}
 
@@ -94,9 +110,7 @@ public class DAO_Admin_Request {
 			try (ResultSet rs = ps.executeQuery()) {
 				if (rs.next()) {
 					status = true;
-					System.out.println(mal.getAdName() + " Admin_Login successful!");
 				} else {
-					System.out.println(mal.getAdName() + " Admin_Login failed!");
 				}
 			}
 		}
@@ -120,9 +134,7 @@ public class DAO_Admin_Request {
 			int row = ps.executeUpdate();
 			if (row > 0) {
 				status = true;
-				System.out.println(matr.getStName() + " Teacher_Registration successful!");
 			} else {
-				System.out.println(matr.getStName() + " Teacher_Registration failed!");
 			}
 		}
 
@@ -145,14 +157,146 @@ public class DAO_Admin_Request {
 			int row = ps.executeUpdate();
 			if (row > 0) {
 				status = true;
-				System.out.println(masr.getStName() + " Student_Registration successful!");
 			} else {
-				System.out.println(masr.getStName() + " Student_Registration failed!");
 			}
 		}
 
 		return status;
 	}
+
+//-----------------------------updateStudent---------------------------------------
+	private static boolean update_Student(MD_Admin_GetList mdastul, Connection reqCon) {
+		boolean status = false;
+		String sql = "UPDATE student_list SET sName = ?, sEmail = ?, sMobile = ?, crId = ?, bName= ?, sMock=?, sWkTst=? WHERE sId = ?";
+
+		try (PreparedStatement ps = reqCon.prepareStatement(sql)) {
+			ps.setString(1, mdastul.getStName());
+			ps.setString(2, mdastul.getStEmail());
+			ps.setString(3, mdastul.getStMobile());
+			ps.setString(4, mdastul.getCrId());
+			ps.setString(5, mdastul.getBname());
+			ps.setInt(6, mdastul.getsMock());
+			ps.setInt(7, mdastul.getsWkTst());
+			ps.setString(8, mdastul.getStId());
+
+			int rows = ps.executeUpdate();
+
+			if (rows > 0) {
+				status = true;
+				System.out.println("..vachindhi....");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return status;
+	}
+
+//-----------------------------updateStudent---------------------------------------
+	private static boolean update_Teacher(MD_Admin_GetList mdastul, Connection reqCon) {
+		boolean status = false;
+		String sql = "UPDATE teacher_list SET tName = ?, tEmail = ?, tMobile = ? WHERE tId = ? AND crId = ?";
+
+		try (PreparedStatement ps = reqCon.prepareStatement(sql)) {
+			ps.setString(1, mdastul.getStName());
+			ps.setString(2, mdastul.getStEmail());
+			ps.setString(3, mdastul.getStMobile());
+			ps.setString(4, mdastul.getStId());
+			ps.setString(5, mdastul.getCrId());
+
+			int rows = ps.executeUpdate();
+			status = rows > 0;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return status;
+	}
+
+//-----------------------------deleteStudent---------------------------------------
+	private static boolean delete_Student(MD_Admin_GetList mdastul, Connection reqCon) {
+		boolean status = false;
+		String sql = "DELETE FROM student_list WHERE sId=? AND crId=?";
+
+		try (PreparedStatement ps = reqCon.prepareStatement(sql)) {
+			ps.setString(1, mdastul.getStId());
+			ps.setString(2, mdastul.getCrId());
+			int rows = ps.executeUpdate();
+
+			if (rows > 0) {
+				status = true;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return status;
+	}
+
+//-----------------------------deleteStudent---------------------------------------
+	private static boolean delete_Teacher(MD_Admin_GetList mdastul, Connection reqCon) {
+		boolean status = false;
+		String sql = "DELETE FROM teacher_list WHERE tId=? AND crId=?";
+
+		System.out.println("Dao vachindhi");
+		try (PreparedStatement ps = reqCon.prepareStatement(sql)) {
+			ps.setString(1, mdastul.getStId());
+			ps.setString(2, mdastul.getCrId());
+
+			int rows = ps.executeUpdate();
+			status = rows > 0;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return status;
+	}
+	
+//------------------------------request_Admin(insert_Teacher)---------------------------------------	
+		private boolean stMW_Insert(MD_Admin_GetList matr, Connection reqCon) throws SQLException {
+			boolean status = false;
+			String sql = "INSERT INTO mockWeekTest_list (mwtMcount,mwtWcount,crId,bName ) VALUES (?,?, ?, ?)";
+
+			try (PreparedStatement ps = reqCon.prepareStatement(sql)) {
+				ps.setLong(1, matr.getAcMockCount());
+				ps.setLong(2, matr.getAcWktstCount());
+				ps.setString(3, matr.getCrId());
+				ps.setString(4, matr.getBname());
+
+				int row = ps.executeUpdate();
+				if (row > 0) {
+					status = true;
+				} else {
+				}
+			}
+
+			return status;
+		}
+		
+//------------------------------request_Admin(insert_Teacher)---------------------------------------	
+				private boolean stMW_Update(MD_Admin_GetList matr, Connection reqCon) throws SQLException {
+					boolean status = false;
+					String sql = "UPDATE mockWeekTest_list SET mwtMcount = ?, mwtWcount = ? WHERE crId = ? AND bName= ?";
+
+					try (PreparedStatement ps = reqCon.prepareStatement(sql)) {
+						ps.setLong(1, matr.getAcMockCount());
+						ps.setLong(2, matr.getAcWktstCount());
+						ps.setString(3, matr.getCrId());
+						ps.setString(4, matr.getBname());
+
+						int row = ps.executeUpdate();
+						if (row > 0) {
+							status = true;
+						} else {
+						}
+					}
+
+					return status;
+				}
 
 //------------------------------retrive_Admin---------------------------------------	
 	public List<MD_Admin_GetList> retrive_Admin(String adType_Ret, String adCid_Ret) {
@@ -164,14 +308,13 @@ public class DAO_Admin_Request {
 
 			retCon = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
 
-			System.out.println(adType_Ret);
-			System.out.println(adCid_Ret);
-
 			if ("teacherList".equalsIgnoreCase(adType_Ret)) {
 				list = getAll_Teacher(retCon, adCid_Ret);
 			} else if ("studentList".equalsIgnoreCase(adType_Ret)) {
 				list = getAll_Student(retCon, adCid_Ret);
-			}else {
+			} else if ("batchList".equalsIgnoreCase(adType_Ret)) {
+				list = getAll_Batch(retCon, adCid_Ret);
+			} else {
 				System.out.println("Admin_Retrive Failed: Unknown Request Type");
 			}
 
@@ -193,11 +336,13 @@ public class DAO_Admin_Request {
 	private List<MD_Admin_GetList> getAll_Teacher(Connection retCon, String adCid_Ret) throws SQLException {
 		List<MD_Admin_GetList> list = new ArrayList<>();
 		String sql = "SELECT * FROM teacher_list WHERE crId = ?";
-
+		DAO_Admin_Request.teacherCount=0;
+		
 		try (PreparedStatement ps = retCon.prepareStatement(sql)) {
 			ps.setString(1, adCid_Ret);
 
 			try (ResultSet rs = ps.executeQuery()) {
+				
 				while (rs.next()) {
 					MD_Admin_GetList madgl_t = new MD_Admin_GetList();
 					madgl_t.setStId(rs.getString("tId"));
@@ -206,6 +351,7 @@ public class DAO_Admin_Request {
 					madgl_t.setStMobile(rs.getString("tMobile"));
 					madgl_t.setCrId(rs.getString("crId"));
 					list.add(madgl_t);
+					DAO_Admin_Request.teacherCount++;
 				}
 			}
 		}
@@ -217,6 +363,7 @@ public class DAO_Admin_Request {
 	private List<MD_Admin_GetList> getAll_Student(Connection retCon, String adCid_Ret) throws SQLException {
 		List<MD_Admin_GetList> list = new ArrayList<>();
 		String sql = "SELECT * FROM student_list WHERE crId = ?";
+		DAO_Admin_Request.studentCount=0;
 
 		try (PreparedStatement ps = retCon.prepareStatement(sql)) {
 			ps.setString(1, adCid_Ret);
@@ -232,12 +379,97 @@ public class DAO_Admin_Request {
 					madgl_t.setBname(rs.getString("bName"));
 					madgl_t.setsMock(rs.getFloat("sMock"));
 					madgl_t.setsWkTst(rs.getFloat("sWkTst"));
+					
+					String sql1 = "SELECT * FROM mockWeekTest_list WHERE  bName = ? AND crId = ?";
+
+					try (PreparedStatement ps1 = retCon.prepareStatement(sql1)) {
+						ps1.setString(1, rs.getString("bName"));
+						ps1.setString(2, adCid_Ret);
+
+						try (ResultSet rs1 = ps1.executeQuery()) {
+							while (rs1.next()) {
+								madgl_t.setAcMockCount(rs1.getInt("mwtMcount"));
+								madgl_t.setAcWktstCount(rs1.getInt("mwtWcount"));
+							}
+						}
+					}
+					
 					list.add(madgl_t);
+					DAO_Admin_Request.studentCount++;
 				}
 			}
 		}
 
 		return list;
 	}
+
+//------------------------------request_Admin(select_Admin)---------------------------------------	
+	private List<MD_Admin_GetList> getAll_Batch(Connection retCon, String adCid_Ret) throws SQLException {
+		List<MD_Admin_GetList> list = new ArrayList<>();
+		String sql = "SELECT * FROM batch_list WHERE crId = ?";
+		DAO_Admin_Request.batchCount=0;
+
+		try (PreparedStatement ps = retCon.prepareStatement(sql)) {
+			ps.setString(1, adCid_Ret);
+
+			try (ResultSet rs = ps.executeQuery()) {
+				while (rs.next()) {
+					MD_Admin_GetList madgl_t = new MD_Admin_GetList();
+					madgl_t.setBname(rs.getString("bName"));
+					madgl_t.setCrId(rs.getString("crId"));
+					
+					String sql1 = "SELECT * FROM student_list WHERE  bName = ? AND crId = ?";
+					int bStudentCount=0;
+
+					try (PreparedStatement ps1 = retCon.prepareStatement(sql1)) {
+						ps1.setString(1, rs.getString("bName"));
+						ps1.setString(2, adCid_Ret);
+
+						try (ResultSet rs1 = ps1.executeQuery()) {
+							while (rs1.next()) {
+								bStudentCount++;
+							}
+						}
+					}
+					madgl_t.setBstudentCount(bStudentCount);
+					
+					String sql2 = "SELECT * FROM mockWeekTest_list WHERE  bName = ? AND crId = ?";
+
+					try (PreparedStatement ps2 = retCon.prepareStatement(sql2)) {
+						ps2.setString(1, rs.getString("bName"));
+						ps2.setString(2, adCid_Ret);
+
+						try (ResultSet rs2 = ps2.executeQuery()) {
+							while (rs2.next()) {
+								madgl_t.setAcMockCount(rs2.getInt("mwtMcount"));
+								madgl_t.setAcWktstCount(rs2.getInt("mwtWcount"));
+							}
+						}
+					}
+					
+					list.add(madgl_t);
+					DAO_Admin_Request.batchCount++;
+				}
+			}
+		}
+		
+
+		return list;
+	}
+	
+	
+	public int getCount(String adc_Type) {
+		int count=0;
+		if("batchCount".equalsIgnoreCase(adc_Type)) {
+			count=batchCount;
+		}else if("teacherCount".equalsIgnoreCase(adc_Type)){
+			count=teacherCount;
+		}else if("studentCount".equalsIgnoreCase(adc_Type)){
+			count=studentCount;
+		}
+		return count;
+		
+	}
+
 
 }
